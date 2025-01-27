49b8755f6396f1faf2e3c69020525de762b0b231
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-27 21:00:16 +0200

    Client: Created Navbar component for students

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>
-        <h1>Navbar</h1>
-    </div>
-  )
-}
+  const isCourseListPage = location.pathname.includes("/course-list");
 
-export default Navbar
+  return (
+    <div
+      className={`flex items-center justify-between px-4 sm:px-10 md:px-14
+    lg-px-36 border-b border-gray-500 py-4 ${
+      isCourseListPage ? "bg-white" : "bg-cyan-100/70"
+    }`}
+    >
+      <img
+        src={assets.logo}
+        alt="Logo"
+        className="w-28 lg:w-32 cursor-pointer"
+      />
+
+      {/* Desktop view */}
+      <div
+        className="hidden md:flex items-center gap-5
+        text-gray-500"
+      >
+        <div>
+          <button>Become Educator</button>|{" "}
+          <Link to="/my-enrollments">My Enrollments</Link>
+        </div>
+        <button
+          className="bg-blue-600 text-white px-5 py-2
+        rounded-full"
+        >
+          Create Account
+        </button>
+      </div>
+
+      {/* Mobile view */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
