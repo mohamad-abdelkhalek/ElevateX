99ae5547817474a3e7a815f2b6cccf49988d09cf
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-01 19:20:53 +0200

    Client: Added course card component

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
 
-const CourseCard = () => {
+const CourseCard = ({ course }) => {
   return (
     <div>
-        <h1>CourseCard</h1>
+      <img src={course.courseThumbnail} alt="Thumbnail" />
+      <div>
+        <h3>{course.courseTitle}</h3>
+        <p>{course.educator.name}</p>
+        <div>
+          <p>4.5</p>
+          <div>
+            {[...Array(5)].map((_, i) => (
+              <img key={i} src={assets.star} alt="Star icon" />
+            ))}
+          </div>
+          <p>22</p>
+        </div>
+        <p>
+          ({course.coursePrice - (course.discount * course.coursePrice) / 100}
+          ).toFixed(2)
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseCard
+export default CourseCard;
