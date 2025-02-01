67c779690b7d4dc7fb85bda307de8d3c9d0a126e
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-01 19:05:07 +0200

    Client: Created courses section

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../../components/student/Hero";
 import Companies from "../../components/student/Companies";
+import CoursesSection from "../../components/student/CoursesSection";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
     >
       <Hero />
       <Companies />
+      <CoursesSection />
     </div>
   );
 };
