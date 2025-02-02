a5e7da593149581e712b08fe544b4077af9bd667
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-02 19:22:35 +0200

    Client: Added read more link button

--- client/src/components/student/Testimonials.jsx
+++ client/src/components/student/Testimonials.jsx
@@ -47,6 +47,9 @@ const Testimonials = () => {
               </div>
               <p className="text-gray-500 mt-5">{testimonial.feedback}</p>
             </div>
+            <a href="#" className="text-blue-500 underline px-5">
+              Read more
+            </a>
           </div>
         ))}
       </div>
