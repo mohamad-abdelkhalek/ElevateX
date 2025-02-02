f5902c8df1f63496aac2518ee73d43ceba79a3e8
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-02 19:44:58 +0200

    Client: Enhanced CallToAction component with responsive design and styling

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -3,15 +3,19 @@ import { assets } from "../../assets/assets";
 
 const CallToAction = () => {
   return (
-    <div>
-      <h1>Learn anything, anytime, anywere</h1>
-      <p>
+    <div className="flex flex-col items-center gap-4 pt-10 pb-24 px-8 md:px-0">
+      <h1 className="text-xl md:text-4xl text-gray-800 font-semibold">
+        Learn anything, anytime, anywere
+      </h1>
+      <p className="text-gray-500 sm:text-sm">
         Unlock your potential with expert-led courses—enroll today and take your
         skills to the next level!
       </p>
-      <div>
-        <button>Get Started</button>
-        <button>
+      <div className="flex items-center font-medium gap-6 mt-4">
+        <button className="px-10 py-3 rounded-md text-white bg-blue-600">
+          Get Started
+        </button>
+        <button className="flex items-center gap-2">
           Learn more <img src={assets.arrow_icon} alt="Arrow icon" />
         </button>
       </div>
