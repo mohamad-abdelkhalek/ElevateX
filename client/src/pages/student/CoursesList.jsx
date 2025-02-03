9193883aeb7ab7431fcf83f88fdb491d9182d404
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-03 17:04:17 +0200

    Client: Implemented CoursesList component with navigation and search

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import SearchBar from "../../components/student/SearchBar";
 
 const CoursesList = () => {
-  return (
-    <div>
-        <h1>Courses List Page</h1>
-    </div>
-  )
-}
+  const { navigate } = useContext(AppContext);
 
-export default CoursesList
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <h1>Courses List</h1>
+            <p className="text-gray-500">
+              <span
+                className="text-blue-600 cursor-pointer"
+                onClick={() => navigate("/")}
+              >
+                Home
+              </span>{" "}
+              / <span>Courses List</span>
+            </p>
+          </div>
+          <SearchBar />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CoursesList;
