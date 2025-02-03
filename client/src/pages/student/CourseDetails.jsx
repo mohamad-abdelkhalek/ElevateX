2cee1ebf89a557a8beba63505dd166bc1bf8f2cf
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-03 19:35:48 +0200

    Client: Implemented course title and truncated description display

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,46 +1,55 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
+import Loading from "../../components/student/Loading";
 
 const CourseDetails = () => {
   const { id } = useParams();
-
-  const [courseData, setCourseData] = useState(null);
-
   const { allCourses } = useContext(AppContext);
 
-  const fetchCourseData = async () => {
-    const findCourse = allCourses.find((course) => course._id === id);
-    setCourseData(findCourse);
+  const courseData = useMemo(
+    () => allCourses.find((course) => course._id === id),
+    [id, allCourses]
+  );
+
+  const truncateText = (text, limit) => {
+    if (!text) return "";
+    if (text.length <= limit) return text;
+    return text.substring(0, text.lastIndexOf(" ", limit)) + "…";
   };
 
-  useEffect(() => {
-    fetchCourseData();
-  }, []);
-
-  return (
-  <div
-    className="flex md:flex-row flex-col-reverse gap-10 relative items-start 
+  return courseData ? (
+    <>
+      <div
+        className="flex md:flex-row flex-col-reverse gap-10 relative items-start 
     justify-between md:px-36 px-8 md:pt-[30px] pt-20 text-left"
-  >
-    {/* Background gradient */}
-    <div
-      className="absolute top-0 left-0 w-full h-[500px] z-[-1] 
+      >
+        {/* Background gradient */}
+        <div
+          className="absolute top-0 left-0 w-full h-[500px] z-[-1] 
       bg-gradient-to-b from-cyan-100/70"
-    ></div>
+        ></div>
 
-    {/* Left Column */}
-    <div>
-      {/* Content goes here */}
-    </div>
+        {/* Left Column */}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="text-[26px] md:text-[44px] font-semibold text-gray-800">
+            {courseData.courseTitle}
+          </h1>
 
-    {/* Right Column */}
-    <div>
-      {/* Content goes here */}
-    </div>
-  </div>
-);
+          <p
+            dangerouslySetInnerHTML={{
+              __html: truncateText(courseData.courseDescription, 200),
+            }}
+          ></p>
+        </div>
 
+        {/* Right Column */}
+        <div>{/* Content goes here */}</div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
 };
 
 export default CourseDetails;
