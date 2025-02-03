26fa25679b31891083b6f6073dde5f76140cb1db
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-03 19:01:28 +0200

    Client: Implemented fetching and displaying individual course data

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
 
 const CourseDetails = () => {
+  const { id } = useParams();
+
+  const [courseData, setCourseData] = useState(null);
+
+  const { allCourses } = useContext(AppContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
+
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+
   return (
     <div>
-        <h1>Course Details Page</h1>
+      <h1>Course Details Page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default CourseDetails
+export default CourseDetails;
