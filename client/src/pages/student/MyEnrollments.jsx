edfc6aefbaafc5f75cfef957d251fd3957f67729
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-08 19:34:21 +0200

    Client: Mounted footer to MyEnrollments page

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import { Line } from "rc-progress";
+import Footer from "../../components/student/Footer";
 
 const MyEnrollments = () => {
   const { enrolledCourses, calculateCourseDuration, navigate } =
@@ -92,6 +93,7 @@ const MyEnrollments = () => {
           </tbody>
         </table>
       </div>
+      <Footer />
     </>
   );
 };
