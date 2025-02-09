f72eaaaf836594770ee36659cca2e8e8b9e2e4f2
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-09 21:12:35 +0200

    Client: Created educator footer

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/educator/Navbar";
 import Sidebar from "../../components/educator/Sidebar";
+import Footer from "../../components/educator/Footer";
 
 const Educator = () => {
   return (
@@ -11,6 +12,7 @@ const Educator = () => {
         <Sidebar />
         <div className="flex-1">{<Outlet />}</div>
       </div>
+      <Footer />
     </div>
   );
 };
