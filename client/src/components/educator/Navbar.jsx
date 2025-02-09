a426221c568957d96d11770695ab8b1b998770e1
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-09 20:32:01 +0200

    Client: Created educator navbar

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { assets, dummyEducatorData } from "../../assets/assets";
+import { UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const educatoraData = dummyEducatorData;
+  const { user } = useUser();
+
   return (
     <div>
-        <h1>Edu Navbar</h1>
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
+      </Link>
+      <div>
+        <p>Hi! {user ? user.fullName : "Developers"}</p>
+        {user ? (
+          <UserButton />
+        ) : (
+          <img className="max-w-8" src={assets.profile_img} />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
