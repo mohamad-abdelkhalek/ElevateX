0dc0377805a2f5c977cce7fb19d3d9332c6973d9
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-13 19:45:21 +0200

    Client: Updated adding lectures popup

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -178,7 +178,104 @@ const AddCourse = () => {
           >
             + Add Chapter
           </div>
+
+          {showPopup && (
+            <div
+              className="fixed inset-0 flex items-center justify-center
+            bg-gray-800 bg-opacity-50"
+            >
+              <div
+                className="bg-white text-gray-700 p-4 rounded relative
+              w-full max-w-80"
+              >
+                <h2 className="text-lg font-semibold mb-4">Add Lecture</h2>
+
+                <div className="mb-2">
+                  <p>Lecture Title</p>
+                  <input
+                    type="text"
+                    className="mt-1 block w-full border rounded py-1 px-2"
+                    value={lectureDetails.lectureTitle}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        lectureTitle: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+
+                <div className="mb-2">
+                  <p>Duration (minutes)</p>
+                  <input
+                    type="number"
+                    className="mt-1 block w-full border rounded py-1 px-2"
+                    value={lectureDetails.lectureDuration}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        lectureDuration: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+
+                <div className="mb-2">
+                  <p>Lecture URL</p>
+                  <input
+                    type="text"
+                    className="mt-1 block w-full border rounded py-1 px-2"
+                    value={lectureDetails.lectureUrl}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        lectureUrl: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+
+                <div className="flex gap-2 my-4">
+                  <p>Is Preview Free?</p>
+                  <input
+                    type="checkbox"
+                    className="mt-1 scale-125"
+                    checked={lectureDetails.isPreviewFree}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        isPreviewFree: e.target.checked,
+                      })
+                    }
+                  />
+                </div>
+
+                <button
+                  type="button"
+                  className="w-full bg-blue-400 text-white
+                  px-4 py-2 rounded"
+                >
+                  Add
+                </button>
+
+                <img
+                  onClick={() => setShowPopup(false)}
+                  src={assets.cross_icon}
+                  className="absolute top-4 right-4 w-4 cursor-pointer"
+                  alt=""
+                />
+              </div>
+            </div>
+          )}
         </div>
+
+        <button
+          type="submit"
+          className="bg-black text-white w-max py-2.5 px-8
+        rounded my-4"
+        >
+          Add
+        </button>
       </form>
     </div>
   );
