e008c1eae61c87a4a7b9d90f791f1a3e1ad2896c
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-13 18:06:29 +0200

    Client: Created basic add course page

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import uniqid from "uniqid";
+import Quill from "quill";
 
 const AddCourse = () => {
-  return (
-    <div>
-        <h1>Add Course</h1>
-    </div>
-  )
-}
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
 
-export default AddCourse
+  const [courseTitle, setCourseTitle] = useState("");
+  const [coursePrice, setCoursePrice] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [image, setImage] = useState(null);
+  const [chapters, setChapters] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [currentChapterId, setCurrentChapterId] = useState(null);
+
+  const [lectureDetails, setLectureDetails] = useState({
+    lectureTitle: "",
+    lectureDuration: "",
+    lectureUrl: "",
+    isPreviewFree: false,
+  });
+
+  useEffect(() => {
+    // Initiate Quill only once
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
+
+  return (
+    <div
+      className="h-screen overflow-scroll flex flex-col items-start
+    justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0"
+    >
+      <h1>Add Course</h1>
+    </div>
+  );
+};
+
+export default AddCourse;
