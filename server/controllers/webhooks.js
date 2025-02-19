dbdf1d681eb156646d8af08c4684e631097f9cca
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-19 18:12:55 +0200

    Server: Added API Controller function to manage Clerk User with database

---
+++ server/controllers/webhooks.js
@@ -0,0 +1,38 @@
+import { Webhook } from "svix";
+import User from "../models/User.js";
+
+// API Controller function to manage Clerk User with database
+const clerkWebhooks = async (req, res) => {
+    try {
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        })
+
+        const {data, type} = req.body
+        
+        switch (type) {
+            case 'user.created': {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+
+                await User.create(userData);
+                res.json({});
+                break;
+            }
+                
+        
+            default:
+                break;
+        }
+    } catch (error) {
+        
+    }
+}
