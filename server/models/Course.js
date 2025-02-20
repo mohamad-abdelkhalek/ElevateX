e2b7588b6410f47a1dabbc26d1d151b1247332e1
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-20 18:58:24 +0200

    Server: Created lecture schema

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
+const lectureSchema = new mongoose.Schema(
+  {
+    lectureId: { type: String, required: true },
+    lectureTitle: { type: String, required: true },
+    lectureDuration: { type: Number, required: true },
+    lectureUrl: { type: String, required: true },
+    isPreviewFree: { type: Boolean, required: true },
+    lectureOrder: { type: Number, required: true },
+  },
+  { _id: false }
+);
+
 const chapterSchema = new mongoose.Schema(
   {
     chapterId: { type: String, required: true },
