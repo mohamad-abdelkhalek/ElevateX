66134cea84b108e6b2734be72cf9ad88e7c2db96
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-22 21:03:48 +0200

    Server: Created controller function to get all courses

---
+++ server/controllers/courseController.js
@@ -0,0 +1,14 @@
+import Course from "../models/Course.js";
+
+// Get all courses
+export const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ isPublished: true })
+      .select(["-courseContent", "-enrolledStudents"])
+      .populate({ path: "educator" });
+
+    res.json({ success: true, courses });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
