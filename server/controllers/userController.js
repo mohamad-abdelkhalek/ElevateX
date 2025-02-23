fad0b298d29019d8506364225232def0196602e7
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-23 19:55:06 +0200

    Server: Imlemented get user data controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,20 @@
+import User from "../models/User.js";
+
+// Get user data
+export const getUserData = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    res.json({ success: true, user });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+
