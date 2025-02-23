e0bb97e5ae2ca6458a134bf50129f9fec3e0064c
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-02-23 20:09:37 +0200

    Server: Fixed user controller error

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,23 +7,31 @@ export const getUserData = async (req, res) => {
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.json({ success: false, message: "User not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
     }
 
     res.json({ success: true, user });
   } catch (error) {
-    res.json({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
 // Users enrolled courses with lecture links
-export const userEnrolledCourses = async () => {
+export const userEnrolledCourses = async (req, res) => {
   try {
     const userId = req.auth.userId;
     const userData = await User.findById(userId).populate("enrolledCourses");
 
+    if (!userData) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
     res.json({ success: true, enrolledCourses: userData.enrolledCourses });
   } catch (error) {
-    res.json({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
